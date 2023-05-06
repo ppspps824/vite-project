8d25b11bfdaba5f027efb3e83830e65372f63ada
papasim824 <example@example.com>
2023-05-06 23:30:00 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
