85f64fb7d447ef519adae08715c8ec5e2d084400
papasim824 <example@example.com>
2023-05-06 15:27:10 +0900

    new

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        lang: 'ja',
+        name: 'sample cam',
+        short_name: 'CAM',
+        background_color: '#fff',
+        theme_color: '#3cb371',
+        display: 'standalone',
+	// GitHub Pagesにpushする場合はリポジトリ名を入れる必要がある
+        scope: '/vite-project/',
+        start_url: '/vite-project/',
+        icons: [
+          {
+            src: 'src/assets/icon.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          // 他のサイズも入れる
+        ]
+      }
+    })
+  ]})
