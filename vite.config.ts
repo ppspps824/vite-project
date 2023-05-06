805d27a8539bef76b875e14b2e5e049989cf3fed
papasim824 <example@example.com>
2023-05-06 15:46:36 +0900

    mod

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES
-  ? "vite-project"
-  : "./",
+  base: "./",
   plugins: [
     react(),
     VitePWA({
